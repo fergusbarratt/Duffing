7b2366bb790e258d6ba83478f4d89887f9684c7c
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2014-03-07 14:56:09 +0000

    Better commenting, Gamma at point chaos starts
    publishes correctly, names of each in each

--- oscil_response.m
+++ oscil_response.m
@@ -1,7 +1,9 @@
+
 close all
 clear all
+
 %% Global Variables
-% define global variables to share with function file
+% define global variables to share with function file oscil_eqn.m
 global gamma omega epsilon GAMMA OMEGA
 % initialise global variables
 omega = 1;
@@ -10,10 +12,12 @@ epsilon = 0.25;
 
 %% Input Variables
 % Two inputs from problem sheet: same capital omega, different capital
-% gamma. SI units assumed for graph labels
+% gamma. SI units assumed for graph labels. Third gamma chosen as point at
+% which chaotic motion about to begin ~ 1.32 to 2d.p
 OMEGA = 2;
 %GAMMA = 0.5;
-GAMMA = 1.5;
+%GAMMA = 1.5;
+GAMMA = 1.32;
 
 %% Solving the ODE
 % Time divisions, T is the period of the driving force
@@ -24,9 +28,9 @@ t_solve_range = 0:deltat:4000;
 [t, x] = ode45(@oscil_eqn, t_solve_range, [1 0]);
 
 %% Plotting
-% find indices for delineating plot ranges. direct matching doesn't work,
-% don't know why. elems is vector of indices of all elements between 20T & 60T
-% inclusive (always inclusive? check- Probably floating point error)
+% find indices for delineating plot ranges. Direct matching doesn't work,
+% probably floating point error. elems is vector of indices of all elements between 20T & 60T
+% inclusive. Very slow, especially for big ranges
 start_find = 20*T;
 end_find = 60*T;
 elems = find(t_solve_range>=start_find & t_solve_range<=end_find);
@@ -40,18 +44,18 @@ x_plot_range = xs(elems);
 %% First Plot
 subplot(3, 1, 1);
 plot(t_plot_range, x_plot_range)
-% set graph properties for first plot of solutions against time
+% Set graph properties for first plot of solutions against time
 title('Time Series');
 ylabel('Displacement/m');
 xlabel('Time/s');
 
 %% Second Plot
-% same trick as before
+% Same trick as before
 start_find = 40*T;
 end_find = 100*T;
 elems = find(t_solve_range>=start_find & t_solve_range<=end_find);
 % Plot ranges
-% ode45 produced two column matrix with first columsn y solutions, 
+% ode45 produced two column matrix with first columns y solutions, 
 % second column dy/dx solutions, slice solutions
 xs = x(:, 1);
 dxdts = x(:, 2);
@@ -59,7 +63,7 @@ x_plot_range = xs(elems);
 dxdt_plot_range = dxdts(elems);
 subplot(3, 1, 2);
 plot(x_plot_range, dxdt_plot_range);
-% set graph properties for phase space trajectory plot window
+% Set graph properties for phase space trajectory plot window
 title('Phase Space Trajectory');
 xlabel('x/m');
 ylabel('dx/dt/ms^-1');
@@ -89,7 +93,7 @@ axis([-3 3 -3 3])
 % the oscillator should return to the same place at the end of each period.
 % Thus GAMMA = 0.5 corresponds to steady state oscillation, with points
 % outside of the chosen range likely a result of transient behaviour
-% for chaotic behavious at GAMMA=1.5 the oscillator ends up in a
+% for chaotic behaviour at GAMMA=1.5 the oscillator ends up in a
 % different place at the end of each period, hence the smearing in the 
 % poincare section
 
